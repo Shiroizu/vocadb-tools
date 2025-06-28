c2ab777b59c9f4ff7edad200d67d8e5c71626e31
Shiroizu <15908050+Shiroizu@users.noreply.github.com>
2025-06-28 10:33:26 +0300

    fix cache key #3 2.3.3

--- packages/vdbpy/src/vdbpy/utils/cache.py
+++ packages/vdbpy/src/vdbpy/utils/cache.py
@@ -14,14 +14,17 @@ def cache_with_expiration(days=1):
     def decorator(func):
         def wrapper(*args, **kwargs):
             # Ignore session parameter:
-            args = [a for a in args if not isinstance(a, Session)]
-            kwargs = {k: v for k, v in kwargs.items() if not isinstance(v, Session)}
-            key = f"{func.__name__}_{args}_{kwargs}"
+
+            cache_args = [a for a in args if not isinstance(a, Session)]
+            cache_kwargs = {k: v for k, v in kwargs.items() if not isinstance(v, Session)}
+            key = f"{func.__name__}_{cache_args}_{cache_kwargs}"
             logger.debug(f"Cache key: {key}")
 
             if key in cache:
-                logger.debug(f"Cache hit for '{key}'")
+                logger.debug(f"Cache hit with '{key}'")
                 return cache[key]
+
+            # Use original args/kwargs to call the function
             result = func(*args, **kwargs)
             cache.set(key, result, expire=timedelta(days=days).total_seconds())
             return result
@@ -35,13 +38,17 @@ def cache_without_expiration():
     def decorator(func):
         def wrapper(*args, **kwargs):
             # Ignore session parameter:
-            args = [a for a in args if not isinstance(a, Session)]
-            kwargs = {k: v for k, v in kwargs.items() if not isinstance(v, Session)}
-            key = f"{func.__name__}_{args}_{kwargs}"
+
+            cache_args = [a for a in args if not isinstance(a, Session)]
+            cache_kwargs = {k: v for k, v in kwargs.items() if not isinstance(v, Session)}
+            key = f"{func.__name__}_{cache_args}_{cache_kwargs}"
             logger.debug(f"Cache key: {key}")
+
             if key in cache:
-                logger.debug(f"Persistent cache hit with '{key}'")
+                logger.debug(f"Cache hit with '{key}'")
                 return cache[key]
+
+            # Use original args/kwargs to call the function
             result = func(*args, **kwargs)
             cache.set(key, result, expire=None)  # No expiration
             return result
@@ -55,14 +62,17 @@ def cache_conditionally(days=1):
     def decorator(func):
         def wrapper(*args, **kwargs):
             # Ignore session parameter:
-            args = [a for a in args if not isinstance(a, Session)]
-            kwargs = {k: v for k, v in kwargs.items() if not isinstance(v, Session)}
-            key = f"{func.__name__}_{args}_{kwargs}"
+
+            cache_args = [a for a in args if not isinstance(a, Session)]
+            cache_kwargs = {k: v for k, v in kwargs.items() if not isinstance(v, Session)}
+            key = f"{func.__name__}_{cache_args}_{cache_kwargs}"
             logger.debug(f"Cache key: {key}")
+
             if key in cache:
                 logger.debug(f"Cache hit with '{key}'")
                 return cache[key]
 
+            # Use original args/kwargs to call the function
             result = func(*args, **kwargs)
 
             if not result:
