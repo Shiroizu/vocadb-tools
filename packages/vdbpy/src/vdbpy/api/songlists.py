65c086a9cfdb80d7db048dd060b264754eb24237
Shiroizu <15908050+Shiroizu@users.noreply.github.com>
2025-07-08 22:39:47 +0300

    rework api/songlist, 3.0.0

--- packages/vdbpy/src/vdbpy/api/songlists.py
+++ packages/vdbpy/src/vdbpy/api/songlists.py
@@ -1,4 +1,5 @@
 import time
+from typing import Literal
 
 from vdbpy.config import WEBSITE
 from vdbpy.utils.logger import get_logger
@@ -6,12 +7,68 @@ from vdbpy.utils.network import fetch_text
 
 logger = get_logger()
 
+Songlist_category = Literal[
+    "Nothing",
+    "Concerts",
+    "VocaloidRanking",
+    "Pools",
+    "Other",
+]
 
-def create_songlists(session, title, song_ids: list[str], max_length=200):
-    """Create songlists on VocaDB with the given title and song IDs.
 
-    Splits the list into sublists if over the max_length.
-    """
+def create_or_update_songlist(
+    session,
+    song_ids: list[int],
+    author_id: int,
+    notes: list[str] | None = None,
+    songlist_id=0,
+    title="",
+    description="",
+    category: Songlist_category = "Pools",
+):
+    """Create or update a songlist."""
+    data = {
+        "songLinks": [],
+        "author": {"id": author_id},
+        "name": title,
+        "featuredCategory": category,
+        "description": description,
+        "status": "Finished",
+        "updateNotes": "",
+    }
+
+    if songlist_id:
+        data["id"] = songlist_id
+
+    order = 1
+    if notes is None:
+        notes = [""] * len(song_ids)
+    for song_id, note in zip(song_ids, notes):
+        songlist_line = {
+            "order": order,
+            "song": {"id": song_id},
+            "notes": note,
+        }
+        data["songLinks"].append(songlist_line)
+        order += 1
+
+    songlist_request = session.post(f"{WEBSITE}/api/songLists", json=data)
+    songlist_request.raise_for_status()
+    if songlist_id:
+        logger.info(f"Updated songlist at {WEBSITE}/SongList/Details/{songlist_id}")
+        logger.debug(f"DATA: {data}")
+    else:
+        songlist_id = songlist_request.json()
+        logger.info(f"Created songlist at {WEBSITE}/SongList/Details/{songlist_id}")
+        logger.debug(f"DATA: {data}")
+
+
+def create_songlists_with_size_limit(
+    session, song_ids: list[int], author_id, title="", max_length=200
+):
+    """Create songlists with a maximum size limit, splitting the songlist into sublists."""
+    if not title.strip():
+        title = "Songlist"
     counter = 1
 
     for i in range(1 + len(song_ids) // max_length):
@@ -21,29 +78,16 @@ def create_songlists(session, title, song_ids: list[str], max_length=200):
 
         if len(song_ids) > max_length:
             logger.info(f"Posting sublist {counter}")
+            create_or_update_songlist(
+                session, sublist, author_id, title=f"{title} #{counter}"
+            )
+            time.sleep(3)
 
         else:
             logger.info("Posting songlist")
+            create_or_update_songlist(session, sublist, author_id, title=title)
 
-        songlist = {
-            "songLinks": [],
-            "author": {"id": 329},
-            "name": f"{title} ({counter})",
-        }
         counter += 1
-
-        order = 1
-        for song_id in sublist:
-            sonlist_item = {"order": order, "song": {"id": int(song_id)}}
-            songlist["songLinks"].append(sonlist_item)
-            order += 1
-
-        songlist_request = session.post(f"{WEBSITE}/api/songLists", json=songlist)
-        songlist_request.raise_for_status()
-        songlist_id = songlist_request.json()
-        logger.info(f"Created songlist at {WEBSITE}/SongList/Details/{songlist_id}")
-
-        time.sleep(3)
 
 
 def export_songlist(songlist_id: int) -> list[str]:
