a77142b33f7fc1764c7253d77ee840de97b105bc
Shiroizu <15908050+Shiroizu@users.noreply.github.com>
2025-07-08 20:23:13 +0300

    fix yt viewcount for deleted videos, 2.8.5

--- packages/vdbpy/src/vdbpy/utils/youtube.py
+++ packages/vdbpy/src/vdbpy/utils/youtube.py
@@ -7,4 +7,6 @@ def get_viewcount(video_id: str, api_key: str) -> int:
     yt_base_url = "https://www.googleapis.com/youtube/v3/videos"
     url = f"{yt_base_url}?part=statistics&id={video_id}&key={api_key}"
     data = fetch_json(url)
+    if not data["items"]:
+        return 0
     return int(data["items"][0]["statistics"]["viewCount"])
