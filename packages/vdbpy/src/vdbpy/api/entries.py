7a6d0cbaa97973490b8dbad88704453e67eb0f57
Shiroizu <15908050+Shiroizu@users.noreply.github.com>
2025-09-25 06:29:08 +0300

    4.16.1: Simplify output & cache get_entry_version

--- packages/vdbpy/src/vdbpy/api/entries.py
+++ packages/vdbpy/src/vdbpy/api/entries.py
@@ -3,16 +3,23 @@ from typing import get_args
 
 from vdbpy.config import WEBSITE
 from vdbpy.types import Edit_type, Entry_type, UserEdit
-from vdbpy.utils.cache import cache_with_expiration
+from vdbpy.utils.cache import cache_with_expiration, cache_without_expiration
 from vdbpy.utils.data import add_s
 from vdbpy.utils.logger import get_logger
 from vdbpy.utils.network import fetch_cached_totalcount, fetch_json
 
 logger = get_logger()
 
-edit_event_map: dict[str, Edit_type] = {"PropertiesUpdated": "Updated", "Deleted": "Deleted", "Created": "Created"}
+edit_event_map: dict[str, Edit_type] = {
+    "PropertiesUpdated": "Updated",
+    "Deleted": "Deleted",
+    "Created": "Created",
+}
 
-def parse_edits_from_archived_versions(data: list[dict], entry_type: Entry_type, entry_id: int) -> list[UserEdit]:
+
+def parse_edits_from_archived_versions(
+    data: list[dict], entry_type: Entry_type, entry_id: int
+) -> list[UserEdit]:
     parsed_edits: list[UserEdit] = []
     for edit_object in data:
         parsed_edits.append(
@@ -29,16 +36,17 @@ def parse_edits_from_archived_versions(data: list[dict], entry_type: Entry_type,
         )
     return parsed_edits
 
+@cache_with_expiration(days=1)
 def get_entry_versions(entry_type: Entry_type, entry_id: int) -> list[UserEdit]:
     url = f"{WEBSITE}/api/{add_s(entry_type)}/{entry_id}/versions"
     data = fetch_json(url)["archivedVersions"]
     return parse_edits_from_archived_versions(data, entry_type, entry_id)
 
-
+@cache_without_expiration()
 def get_entry_version(entry_type: Entry_type, version_id: int) -> dict:
     # TODO proper return types: Song | Album | ..
     url = f"{WEBSITE}/api/{add_s(entry_type)}/versions/{version_id}"
-    return fetch_json(url)
+    return fetch_json(url)["versions"]["firstData"]
 
 
 @cache_with_expiration(days=1)
@@ -65,13 +73,14 @@ def is_deleted(entry_type: Entry_type, entry_id: int) -> bool:
         return entry["deleted"]
     return False
 
+
 def delete_entry(
     session,
     entry_type: Entry_type,
     entry_id: int,
     force=False,
     deletion_msg="",
-    prompt=True
+    prompt=True,
 ) -> bool:
     if is_deleted(entry_type, entry_id):
         logger.warning(f"Entry {entry_id} has already been deleted.")
@@ -94,4 +103,3 @@ def delete_entry(
     deletion_attempt.raise_for_status()
 
     return True
-
