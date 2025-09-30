cdbfb00616cd7f022ceaedba5d4924048a943704
Shiroizu <15908050+Shiroizu@users.noreply.github.com>
2025-10-01 01:11:57 +0300

    favourite producers: include albums

--- packages/vocadb-scripts/US_find_favourite_producers.py
+++ packages/vocadb-scripts/US_find_favourite_producers.py
@@ -1,38 +1,15 @@
-"""Find favourite albums for user based on rated songs. Example output below.
+"""Generate a table of user's favourite producers."""
 
-|   Score |   Favs |   Likes |   Favs/Likes |   Rated % | Artist           | Entry                                       |
-|---------|--------|---------|--------------|-----------|------------------|---------------------------------------------|
-|     263 |     59 |      43 |          1.4 |      32.5 | Kuroneko Lounge  | https://vocadb.net/Ar/1063                  |
-|     231 |     73 |       6 |         12.2 |      23.7 | Clean Tears      | https://vocadb.net/Ar/20                    |
-|     208 |     58 |      17 |          3.4 |      44.4 | TKN              | https://vocadb.net/Ar/19059                 |
-|     182 |     44 |      25 |          1.8 |        23 | オカメP          | https://vocadb.net/Ar/87                    |
-|     154 |     50 |       2 |           25 |      73.2 | 鼻そうめんP      | https://vocadb.net/Ar/175                   |
-|     130 |     28 |      23 |          1.2 |      36.7 | Osanzi           | https://vocadb.net/Ar/18777                 |
-|     124 |     26 |      23 |          1.1 |      40.5 | LIQ              | https://vocadb.net/Ar/88                    |
-|     103 |     29 |       8 |          3.6 |      38.5 | Alpaca           | https://vocadb.net/Ar/44866                 |
-|     100 |     22 |      17 |          1.3 |      13.7 | 鬱P              | https://vocadb.net/Ar/90 (not following)    |
-|      98 |     22 |      16 |          1.4 |      30.2 | Twinfield        | https://vocadb.net/Ar/4828                  |
-|      93 |     27 |       6 |          4.5 |      56.9 | ddh              | https://vocadb.net/Ar/4724                  |
-|      92 |     20 |      16 |          1.2 |      16.4 | 八王子P          | https://vocadb.net/Ar/38                    |
-|      92 |     14 |      25 |          0.6 |      24.1 | 雄之助           | https://vocadb.net/Ar/23981                 |
-|      90 |     16 |      21 |          0.8 |      28.2 | PLAMA            | https://vocadb.net/Ar/46360                 |
-|      81 |     19 |      12 |          1.6 |         9 | Transient Energy | https://vocadb.net/Ar/16023                 |
-|      78 |     12 |      21 |          0.6 |      42.3 | 攻               | https://vocadb.net/Ar/13271 (not following) |
-|      74 |     16 |      13 |          1.2 |       8.3 | AVTechNO!        | https://vocadb.net/Ar/40                    |
-|      71 |     19 |       7 |          2.7 |      14.6 | Spacelectro      | https://vocadb.net/Ar/11722                 |
-|      71 |     17 |      10 |          1.7 |      20.8 | Mwk              | https://vocadb.net/Ar/16866                 |
-|      70 |     14 |      14 |            1 |        23 | Giga             | https://vocadb.net/Ar/772                   |
-
-Table saved to 'output/favourite-producers-329.txt'
-"""
-
-# TODO add --exclude_new_songs
+# TODO youtube channel
+# TODO include most popular song
+# TODO include latest song publish date
 
 import argparse
 
 from tabulate import tabulate
 from vdbpy.api.artists import get_song_count_by_artist_id
 from vdbpy.api.users import (
+    get_albums_by_user_id,
     get_followed_artists_by_user_id,
     get_rated_songs_by_user_id,
     get_username_by_id,
@@ -42,6 +19,158 @@ from vdbpy.utils.files import save_file
 from vdbpy.utils.logger import get_logger
 
 logger = get_logger("find-favourite-producers")
+
+
+def find_favourite_producers_by_user_id(user_id: int, max_results):
+    username = get_username_by_id(user_id)
+    logger.info(f"Searching favourite producers for user '{username}' ({user_id})")
+
+    unique_artists = {}
+
+    fields = [
+        "Albums",
+        "Artists",
+        "PVs",
+        "ReleaseEvent",
+        "Tags",
+        "WebLinks",
+        "CultureCodes",
+    ]
+
+    params = {"fields": ", ".join(fields)}
+    rated_songs = get_rated_songs_by_user_id(user_id, params)
+    # TODO permanent* cache
+
+    for song in rated_songs:
+        placeholder = ""
+        rating = song["rating"]
+        try:
+            for artist in song["song"]["artists"]:
+                if "Producer" in artist["categories"]:
+                    placeholder = artist["name"]
+                    artist_id = artist["artist"]["id"]
+                    if artist_id in unique_artists:
+                        if rating == "Favorite":
+                            unique_artists[artist_id][1] += 1
+                        elif rating == "Like":
+                            unique_artists[artist_id][2] += 1
+                    elif rating == "Favorite":
+                        unique_artists[artist_id] = [
+                            artist["artist"]["name"],
+                            1,
+                            0,
+                            0,
+                            0,
+                        ]
+                    elif rating == "Like":
+                        unique_artists[artist_id] = [
+                            artist["artist"]["name"],
+                            0,
+                            1,
+                            0,
+                            0,
+                        ]
+
+        except KeyError:
+            logger.debug(f"Custom artist '{placeholder}' on S/{song['song']['id']}")
+            continue
+
+    albums = get_albums_by_user_id(user_id, extra_params={"fields": "Artists, Tracks"})
+    for album in albums:
+        album_producers_ids_by_name = {
+            artist["artist"]["name"]: artist["artist"]["id"]
+            for artist in album["album"]["artists"]
+            if artist["categories"] == "Producer" and "artist" in artist
+        }
+        for artist in album["album"]["artists"]:
+            if artist["categories"] != "Producer" or artist["isSupport"]:
+                continue
+            if artist["artist"]["id"] in unique_artists:
+                unique_artists[artist["artist"]["id"]][3] += 1
+            else:
+                unique_artists[artist["artist"]["id"]] = [
+                    artist["artist"]["name"],
+                    0,
+                    0,
+                    1,
+                    0,
+                ]
+        for track in album["album"]["tracks"]:
+            artist_name_from_artist_string = track["song"]["artistString"].split(
+                " feat. "
+            )[0]
+            artist_id = album_producers_ids_by_name.get(
+                artist_name_from_artist_string, 0
+            )
+            if artist_id in unique_artists:
+                unique_artists[artist_id][4] += 1
+            else:
+                logger.info(
+                    f"Album '{album['album']['name']}': Found an unknown tracklist artist '{artist_name_from_artist_string}'"
+                )
+                logger.info(f"Album producers: {album_producers_ids_by_name}")
+    logger.info(f"Analyzed {len(albums)} albums...")
+    logger.info(f"Found {len(unique_artists)} unique artists...")
+
+    unique_artists_with_score = []
+    for ar_id in unique_artists:
+        name, favs, likes, album_count, album_track_count = [
+            unique_artists[ar_id][0],
+            unique_artists[ar_id][1],
+            unique_artists[ar_id][2],
+            unique_artists[ar_id][3],
+            unique_artists[ar_id][4],
+        ]
+        score = favs * 3 + likes * 2 + 10 * album_count + album_track_count
+        unique_artists_with_score.append(
+            [name, favs, likes, score, ar_id, album_count, album_track_count]
+        )
+
+    unique_artists_with_score.sort(key=lambda x: x[3], reverse=True)
+
+    followed_artists = []
+    logger.info("Fetching followed artists...")
+    followed_artists = get_followed_artists_by_user_id(user_id)
+    followed_artists_ids = [int(artist["id"]) for artist in followed_artists]
+
+    table_to_print = []
+    for ar in unique_artists_with_score[:max_results]:
+        logger.info(f"Generating row for artist {ar}...")
+        following = False
+        name, favs, likes, score, ar_id, album_count, album_track_count = ar
+        songcount_by_artist = get_song_count_by_artist_id(
+            int(ar_id), only_main_songs=True
+        )
+        rated_songs_percentage = round(((favs + likes) / songcount_by_artist * 100), 1)
+        if int(ar_id) in followed_artists_ids:
+            following = True
+        headers = [
+            "Score",
+            "Favs",
+            "Likes",
+            "Favs/Likes",
+            "Rated %",
+            "Album count",
+            "Album track count",
+            "Artist",
+            "Following",
+            "Entry",
+        ]
+        line_to_print = (
+            score,
+            favs,
+            likes,
+            round(favs / likes, 1) if likes else "",
+            rated_songs_percentage,
+            album_count,
+            album_track_count,
+            name,
+            following,
+            f"{WEBSITE}/Ar/{ar_id}",
+        )
+        table_to_print.append(line_to_print)
+
+    return headers, table_to_print
 
 
 def parse_args() -> argparse.Namespace:
@@ -65,97 +194,12 @@ if __name__ == "__main__":
     args = parse_args()
     user_id = args.user_id
     max_results = args.max_results
-
-    username = get_username_by_id(user_id)
-    logger.info(f"Searching favourite producers for user '{username}' ({user_id})")
-
     OUTPUT_FILE = f"output/favourite-producers-{user_id}.csv"
-
-    unique_artists = {}
-
-    fields = [
-        "Albums",
-        "Artists",
-        "PVs",
-        "ReleaseEvent",
-        "Tags",
-        "WebLinks",
-        "CultureCodes",
-    ]
-
-    params = {"fields": ", ".join(fields)}
-    rated_songs = get_rated_songs_by_user_id(user_id, params)
-
-    for song in rated_songs:
-        placeholder = ""
-        rating = song["rating"]
-        try:
-            for artist in song["song"]["artists"]:
-                if "Producer" in artist["categories"]:
-                    placeholder = artist["name"]
-                    artist_id = artist["artist"]["id"]
-                    if artist_id in unique_artists:
-                        if rating == "Favorite":
-                            unique_artists[artist_id][1] += 1
-                        elif rating == "Like":
-                            unique_artists[artist_id][2] += 1
-                    elif rating == "Favorite":
-                        unique_artists[artist_id] = [artist["artist"]["name"], 1, 0]
-                    elif rating == "Like":
-                        unique_artists[artist_id] = [artist["artist"]["name"], 0, 1]
-
-        except KeyError:
-            logger.debug(f"Custom artist '{placeholder}' on S/{song['song']['id']}")
-            continue
-
-    unique_artists_with_score = []
-
-    for ar_id in unique_artists:
-        name, favs, likes = [
-            unique_artists[ar_id][0],
-            unique_artists[ar_id][1],
-            unique_artists[ar_id][2],
-        ]
-        score = favs * 3 + likes * 2
-        unique_artists_with_score.append([name, favs, likes, score, ar_id])
-
-    unique_artists_with_score.sort(key=lambda x: x[3], reverse=True)
-
-    followed_artists = []
-    page = 1
-
-    followed_artists = get_followed_artists_by_user_id(user_id)
-    followed_artists_ids = [int(artist["id"]) for artist in followed_artists]
-
-    if not max_results:
-        max_results = None
-
-    table_to_print = []
-    for ar in unique_artists_with_score[:max_results]:
-        follow_msg = "(not following)"
-        name, favs, likes, score, ar_id = ar
-        songcount_by_artist = get_song_count_by_artist_id(
-            int(ar_id), only_main_songs=True
-        )
-        rated_songs_percentage = round(((favs + likes) / songcount_by_artist * 100), 1)
-        if int(ar_id) in followed_artists_ids:
-            follow_msg = ""
-        headers = ["Score", "Favs", "Likes", "Favs/Likes", "Rated %", "Artist", "Entry"]
-        line_to_print = (
-            score,
-            favs,
-            likes,
-            round(favs / likes, 1) if likes else "",
-            rated_songs_percentage,
-            name,
-            f"{WEBSITE}//Ar/{ar_id} {follow_msg}",
-        )
-        table_to_print.append(line_to_print)
-
+    headers, producer_table = find_favourite_producers_by_user_id(user_id, max_results)
+    logger.info(f"Got {len(producer_table)} producers...")
     table = tabulate(
-        table_to_print, headers=headers, tablefmt="github", numalign="right"
+        producer_table, headers=headers, tablefmt="github", numalign="right"
     )
     logger.info(f"\n{table}")
-
     save_file(OUTPUT_FILE, table)
     logger.info(f"\nTable saved to '{OUTPUT_FILE}'")
