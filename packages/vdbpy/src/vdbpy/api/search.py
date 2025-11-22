faf6a44f3996d53e8a58329154c811364e839414
Shiroizu <15908050+Shiroizu@users.noreply.github.com>
2025-11-22 18:23:28 +0200

    rework  search_entries*

--- packages/vdbpy/src/vdbpy/api/search.py
+++ packages/vdbpy/src/vdbpy/api/search.py
@@ -20,9 +20,12 @@ from vdbpy.types.shared import EntryType
 # TODO save results by copypasting browser url
 
 
-def search_entry_ids(
-    name: str, entry_type: EntryType, max_results: int = 3
-) -> tuple[list[int], int]:
+def search_entries(
+    name: str,
+    entry_type: EntryType,
+    max_results: int = 3,
+    extra_params: dict[Any, Any] | None = None,
+) -> tuple[list[Any], int]:
     search_functions: dict[
         EntryType, tuple[Callable[..., tuple[list[Any], int]], str]
     ] = {
@@ -42,6 +45,10 @@ def search_entry_ids(
         "getTotalCount": True,
         "query": name,
     }
+    if extra_params:
+        for field, value in extra_params.items():
+            if field not in params:
+                params[field] = value
     search_function, sort_rule = search_functions[entry_type]
     params["sort"] = sort_rule
     results, total_count = search_function(params, max_results=max_results)
@@ -50,15 +57,15 @@ def search_entry_ids(
         results, total_count = search_function(params, max_results=max_results)
     if not results:
         return [], 0
-    entry_ids = [int(entry["id"]) for entry in results]
-    return entry_ids, total_count
+    return results, total_count
 
 
-def search_entry(name: str, entry_type: EntryType, max_results: int = 3) -> str:
-    entry_ids, total_count = search_entry_ids(name, entry_type, max_results)
-    if not entry_ids:
+def search_entry_links(name: str, entry_type: EntryType, max_results: int = 3) -> str:
+    entries, total_count = search_entries(name, entry_type, max_results)
+    if not entries:
         return f"No results found for '{name}'"
 
+    entry_ids = [int(entry["id"]) for entry in entries]
     links = [
         get_entry_link(entry_type, entry_id) for entry_id in entry_ids[:max_results]
     ]
